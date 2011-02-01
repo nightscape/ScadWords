4a8f82da3f7a4ea7978a198c304df39c763164b7
Martin Mauch <mauch@crealytics.de>
2011-02-01 10:22:10 +0100

    Started implementing createCampaign.

--- src/main/scala/RichAccount.scala
+++ src/main/scala/RichAccount.scala
@@ -1,6 +1,8 @@
 import com.google.api.adwords.v201008.mcm.Account
+import com.google.api.adwords.v201008.cm.Campaign
 class RichAccount(val account:Account) {
   def createCampaign(name:String) = {
-	  
+    val campaign = new Campaign()
+    campaign.setName(name)
   }
 }
